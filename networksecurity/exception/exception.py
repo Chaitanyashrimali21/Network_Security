741f3ad4d467523cbc40f380354427d806fb56f9
Chaitanya Shrimali <chaitanyashrimali7322@gmail.com>
2025-09-25 22:29:41 +0530

    add empty __init__.py files for package initialization

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,20 @@
+import sys
+
+from networksecurity.logging import logger
+
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, err_detail: sys):
+        self.error_message = error_message
+        _, _, exc_tb = err_detail.exc_info()
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return f"Error occured in script: {self.file_name} at line number: {self.lineno} error message: {self.error_message}"
+
+# if __name__ == "__main__":
+#     try:
+#         logger.logging.info("Logging has started")
+#         a = 1/0
+#     except Exception as e:
+#         raise NetworkSecurityException(e, sys)
