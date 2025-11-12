d22b51a090da4bc0c43e35c7ed09646f72588dc3
Chaitanya Shrimali <chaitanyashrimali7322@gmail.com>
2025-11-12 21:51:30 +0530

    Add model trainer component and integrate into main pipeline

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from networksecurity.entity.config_entity import DataIngestionConfig, DataValida
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
-from networksecurity.entity.config_entity import DataValidationConfig,DataIngestionConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataValidationConfig,DataIngestionConfig,DataTransformationConfig,ModelTrainerConfig
+from networksecurity.components.model_trainer import ModelTrainer
 import sys
 
 if __name__ == "__main__":
@@ -29,5 +30,11 @@ if __name__ == "__main__":
       data_transformation_artifact = data_transformation.initiate_data_transformation()
       print(data_transformation_artifact)
       logging.info("completed data transformation")
+      logging.info("initiated model trainer")
+      model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+      model_trainer = ModelTrainer(model_trainer_config = model_trainer_config,data_transformation_artifact =data_transformation_artifact)
+      model_trainer_artifact = model_trainer.initiate_model_trainer()
+      logging.info("completed model trainer")
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 
-TARGET_COLUMN = "result"
+TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifact"
 FILE_NAME: str = "PhishingData.csv"
@@ -11,6 +11,9 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
+SAVED_MODEL_DIR =os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
 
 
 """Data Ingestion related entities"""
@@ -41,3 +44,19 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "n_neighbors": 3,
     "weights": "uniform",
 }
+DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
+
+DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
+
+
+"""
+Model Trainer ralated constant start with MODE TRAINER VAR NAME
+"""
+
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
+MODEL_TRAINER_OVER_FIITING_UNDER_FITTING_THRESHOLD: float = 0.05
+
+TRAINING_BUCKET_NAME = "netwworksecurity"
